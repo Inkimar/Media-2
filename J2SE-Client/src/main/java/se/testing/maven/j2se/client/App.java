2c2eed78baead7aad1980e675ac76609f52cd310
unknown <Ingimar@Ingimar-WorkLap.(none)>
2013-08-14 14:24:45 +0200

    a kdkd

--- J2SE-Client/src/main/java/se/testing/maven/j2se/client/App.java
+++ J2SE-Client/src/main/java/se/testing/maven/j2se/client/App.java
@@ -24,12 +24,20 @@ public class App {
         System.out.println("END: Hej världen!");
     }
 
+    /**
+     * https://forums.oracle.com/thread/2205656
+     * Fails on Windows :
+     * - using Glassfish 3.1.2.2
+     * @return
+     * @throws NamingException 
+     */
     private static MediaService fetchBean() throws NamingException {
         Properties jndiProps = new Properties();
         String ipAtHome = " 192.168.10.163";
         jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", ipAtHome); // 172.16.34.31 , 192.168.10.163 , 127.0.0.1
         Context ctx = new InitialContext(jndiProps);
         String name="java:global/se.nrm.mediaserver_MediaserverApp-ejb_ejb_1.0-SNAPSHOT/MediaServiceBean!se.nrm.mediaserver.domain.MediaService";
+        String shortName="java:global/se.nrm.mediaserver_MediaserverApp-ejb_ejb_1.0-SNAPSHOT/MediaServiceBean";
         String orgName="java:global/MediaserverApp-ejb/MediaServiceBean";
         MediaService bean = (MediaService) ctx.lookup(name);
         return bean;
